f248e34b45c70f2534c3a4f92a34628d3c9895c5
Saydaliev <62750010+Saydaliev@users.noreply.github.com>
2020-04-01 12:14:59 +0600

    Add files via upload

---
+++ Project2.R
@@ -0,0 +1,112 @@
+setwd("~/Documents/DataScienceCoursera/Exploratory Data Analysis")
+# Import Data into R studio
+library("data.table")
+
+NEI <- data.table::as.data.table(x=readRDS(file = "summarySCC_PM25.rds"))
+SCC <- data.table::as.data.table(x=readRDS(file="Source_Classification_Code.rds"))
+
+#Question 1: Plot1
+
+NEI[, Emissions := lapply(.SD, as.numeric), .SDcols = c("Emissions")]
+
+totalNEI <- NEI[, lapply(.SD, sum, na.rm = TRUE), .SDcols = c("Emissions"), by = year]
+
+png(filename='project2plot1.png')
+
+barplot(totalNEI[, Emissions]
+        , names = totalNEI[, year]
+        , xlab = "Years", ylab = "Emissions"
+        , main = "Emissions over the Years")
+
+dev.off()
+
+##**Question 2 (Plot2)
+
+NEI[, Emissions := lapply(.SD, as.numeric), .SDcols = c("Emissions")]
+totalNEI <- NEI[fips=='24510', lapply(.SD, sum, na.rm = TRUE)
+                , .SDcols = c("Emissions")
+                , by = year]
+
+png(filename='Project2plot2.png')
+
+barplot(totalNEI[, Emissions]
+        , names = totalNEI[, year]
+        , xlab = "Years", ylab = "Emissions"
+        , main = "Emissions over the Years")
+
+dev.off()
+
+###**Question3 (Plot3)
+baltimoreNEI <- NEI[fips=="24510",]
+
+png("Project2plot3.png")
+
+library('ggplot2')
+
+ggplot(baltimoreNEI,aes(factor(year),Emissions,fill=type)) +
+  geom_bar(stat="identity") +
+  facet_grid(.~type,scales = "free",space="free") + 
+  labs(x="year", y=expression("Total PM"[2.5]*" Emission (Tons)")) + 
+  labs(title=expression("PM"[2.5]*" Emissions, Baltimore City 1999-2008 by Source Type"))
+
+dev.off()
+
+
+#**Question 4 ( Plot 5)
+ 
+combustionRelated <- grepl("comb", SCC[, SCC.Level.One], ignore.case=TRUE)
+coalRelated <- grepl("coal", SCC[, SCC.Level.Four], ignore.case=TRUE) 
+combustionSCC <- SCC[combustionRelated & coalRelated, SCC]
+combustionNEI <- NEI[NEI[,SCC] %in% combustionSCC]
+
+png("Project2plot4.png")
+
+ggplot(combustionNEI,aes(x = factor(year),y = Emissions/10^5)) +
+  geom_bar(stat="identity", fill ="#FF9999", width=0.75) +
+  labs(x="year", y=expression("Total PM"[2.5]*" Emission (10^5 Tons)")) + 
+  labs(title=expression("PM"[2.5]*" Coal Combustion Source Emissions Across US from 1999-2008"))
+
+dev.off()
+
+#**Question 5 (Plot 5)**
+condition <- grepl("vehicle", SCC[, SCC.Level.Two], ignore.case=TRUE)
+vehiclesSCC <- SCC[condition, SCC]
+vehiclesNEI <- NEI[NEI[, SCC] %in% vehiclesSCC,]
+
+# Subset the vehicles NEI data to Baltimore's fip
+baltimoreVehiclesNEI <- vehiclesNEI[fips=="24510",]
+
+png("Project2plot5.png")
+
+ggplot(baltimoreVehiclesNEI,aes(factor(year),Emissions)) +
+  geom_bar(stat="identity", fill ="#FF9999" ,width=0.75) +
+  labs(x="year", y=expression("Total PM"[2.5]*" Emission (10^5 Tons)")) + 
+  labs(title=expression("PM"[2.5]*" Motor Vehicle Source Emissions in Baltimore from 1999-2008"))
+
+dev.off()
+
+#**Question6 (Plot6)**
+
+condition <- grepl("vehicle", SCC[, SCC.Level.Two], ignore.case=TRUE)
+vehiclesSCC <- SCC[condition, SCC]
+vehiclesNEI <- NEI[NEI[, SCC] %in% vehiclesSCC,]
+
+# Subset the vehicles NEI data by each city's fip and add city name.
+vehiclesBaltimoreNEI <- vehiclesNEI[fips == "24510",]
+vehiclesBaltimoreNEI[, city := c("Baltimore City")]
+
+vehiclesLANEI <- vehiclesNEI[fips == "06037",]
+vehiclesLANEI[, city := c("Los Angeles")]
+
+# Combine data.tables into one data.table
+bothNEI <- rbind(vehiclesBaltimoreNEI,vehiclesLANEI)
+
+png("Project2plot6.png")
+
+ggplot(bothNEI, aes(x=factor(year), y=Emissions, fill=city)) +
+  geom_bar(aes(fill=year),stat="identity") +
+  facet_grid(scales="free", space="free", .~city) +
+  labs(x="year", y=expression("Total PM"[2.5]*" Emission (Kilo-Tons)")) + 
+  labs(title=expression("PM"[2.5]*" Motor Vehicle Source Emissions in Baltimore & LA, 1999-2008"))
+
+dev.off()
